14cef61f06ebbf1850d0b9306fa3b220530d2c61
rupink <rupin_23@hotmail.com>
2023-10-26 13:01:15 -0400

    README added

---
+++ README.d
@@ -0,0 +1,13 @@
+# ECE444-F2023-Lab5
+
+replicate of https://github.com/mjhea0/flaskr-tdd/tree/main
+
+## Pros and Cons of TDD:
+
+Test-Driven Development (TDD) offers a distinctive approach to software development that boasts both merits and drawbacks. TDD's primary advantage lies in its proactive stance toward software quality. It places testing at the forefront, compelling developers to consider design and architecture deeply. This emphasis on sound planning often results in cleaner, more maintainable code. TDD's early bug detection and regression prevention translate to minimized debugging time and increased confidence in code refactoring. Moreover, it keeps development teams firmly on the path to meeting project requirements.
+
+Another hidden treasure within TDD is the test suite itself, which functions as valuable documentation. It aligns the code closely with desired functionality and facilitates parallel development. Although TDD demands an initial investment of time and effort, the long-term benefits make it a valuable asset for producing robust software.
+
+On the flip side, TDD presents certain challenges. Some view it as an overhead due to the additional work of writing and maintaining test code. TDD may not seamlessly fit all projects, especially those with vague or evolving requirements. Test fragility can emerge if tests become overly entangled with code implementation details, leading to frequent updates.
+
+Additionally, TDD does not offer an all-encompassing shield against defects, potentially fostering a false sense of security. Overcoming the learning curve and potential resistance to change among team members may also pose adoption barriers. Despite these drawbacks, TDD remains a potent practice for elevating code quality and reducing bugs, provided it's employed within the right context.
